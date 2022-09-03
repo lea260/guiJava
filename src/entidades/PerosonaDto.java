8143854fcda799f44e3a5a28176aa471acada29e
Lautaro Mendez <lautaro_123mendez@hotmail.com>
2022-09-03 16:59:28 -0300

    agregado de singleton en persistencia

--- src/entidades/PerosonaDto.java
+++ src/entidades/PerosonaDto.java
@@ -4,6 +4,7 @@ public class PerosonaDto {
 	private int id;
 	private int edad;
 	private String nombre;
+
 	public PerosonaDto(int id, int edad, String nombre) {
 		super();
 		this.id = id;
--- src/persistencia/Singleton.java
+++ src/persistencia/Singleton.java
@@ -2,8 +2,27 @@ package persistencia;
 
 import java.util.ArrayList;
 
+import entidades.PerosonaDto;
+
 public class Singleton {
-	private static Singleton instacia = null;
-	private ArrayList<Integer> listaP;
+	private static Singleton instancia = null;
+	private ArrayList<PerosonaDto> listaP;
+
+	private Singleton() {
+
+	}
+
+	public static Singleton getInstancia() {
+		if (instancia == null) {
+			instancia = new Singleton();
+			instancia.listaP = new ArrayList<PerosonaDto>();
+			instancia.listaP.add(new PerosonaDto(1, 25, "Juan"));
+			instancia.listaP.add(new PerosonaDto(2, 66, "Pedro"));
+			instancia.listaP.add(new PerosonaDto(1, 25, "Maria"));
+			instancia.listaP.add(new PerosonaDto(1, 25, "Sofia"));
+
+		}
+		return instancia;
+	}
 
 }
